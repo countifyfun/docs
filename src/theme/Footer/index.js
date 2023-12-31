1e469b597e3557c056ee6a5b68fd36e5271cafd4
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-31 16:46:42 +0800

    feat(styles): consistent with main site

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./index.module.css";
+
+function Footer() {
+  return (
+    <footer aria-labelledby="footer-heading" className={styles.footer}>
+      <div>
+        <a href="https://countify.fun" className={styles.footer_name_container}>
+          <img
+            src="https://countify.fun/logo.png"
+            alt="Countify Logo"
+            width={35}
+            height={35}
+            className={styles.footer_image}
+          />
+          <h1 className={styles.footer_name}>Countify</h1>
+        </a>
+        <p className={styles.footer_copyright}>
+          © 2023 <a href="https://github.com/countifyfun">Countify Labs</a> (a
+          subdivision of{" "}
+          <a href="https://youtube.com/@graphifystatistics">Graphify Studios</a>
+          ). All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+export default React.memo(Footer);
